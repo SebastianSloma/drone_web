4bf588a49a70492212e069d2e71eb92c4a1a3bae
Sebastian Słoma <sebastiansloma@gmail.com>
2024-04-30 18:05:07 +0200

    added nav bar menu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss';
-
+const {nextui} = require("@nextui-org/react");
 const {
 	default: flattenColorPalette,
 } = require('tailwindcss/lib/util/flattenColorPalette');
@@ -9,15 +9,17 @@ const config: Config = {
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 	],
 	darkMode: 'class',
+  
 	theme: {
 		extend: {
 			backgroundImage: {},
 		},
 		plugins: [addVariablesForColors],
 	},
-	plugins: [],
+  plugins: [nextui()],
 };
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
