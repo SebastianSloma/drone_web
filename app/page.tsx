92f1216cfb9a648d7daff84f58a5f8173d14fcbe
Sebastian Słoma <sebastiansloma@gmail.com>
2024-04-30 16:57:59 +0200

    ad pin comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Image from 'next/image';
+
 
 export default function Home() {
 	return (<div>
 <h1>testy</h1>
+<h2 className='text-slate-400'>ocdnosdncosd</h2>
   </div>
+  
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,35 @@
+import type { Config } from 'tailwindcss';
+
 const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
+	default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  plugins: [addVariablesForColors],
+const config: Config = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			backgroundImage: {},
+		},
+		plugins: [addVariablesForColors],
+	},
+	plugins: [],
 };
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
 function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
+	let allColors = flattenColorPalette(theme('colors'));
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
 
-  addBase({
-    ":root": newVars,
-  });
+	addBase({
+		':root': newVars,
+	});
 }
+
+export default config;
