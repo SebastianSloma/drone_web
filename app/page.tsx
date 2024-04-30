441dab2452cdaafea22ba007557f20f0b145cabd
Sebastian Słoma <sebastiansloma@gmail.com>
2024-04-30 17:28:37 +0200

    added menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { NavbarDemo } from './components/navbar';
 
 
 export default function Home() {
 	return (<div>
 <h1>testy</h1>
 <h2 className='text-slate-400'>ocdnosdncosd</h2>
+<NavbarDemo/>
   </div>
   
   )
