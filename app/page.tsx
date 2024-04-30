f5fc8c594f758a7b6776bc83d6a5ca2f07d0680f
Sebastian Słoma <sebastiansloma@gmail.com>
2024-04-30 17:41:10 +0200

    added next ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import { NavbarDemo } from './components/navbar';
+import { AnimatedPinDemo } from './components/3dpin';
+
 
 
 export default function Home() {
 	return (<div>
 <h1>testy</h1>
 <h2 className='text-slate-400'>ocdnosdncosd</h2>
-<NavbarDemo/>
+
+
+<div>
+  <AnimatedPinDemo/>
+</div>
   </div>
   
   )
