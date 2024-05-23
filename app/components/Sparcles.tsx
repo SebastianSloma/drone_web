4402eccba7348991ae2bb30ce92bab4bfcaff83c
Sebastian Słoma <sebastiansloma@gmail.com>
2024-05-23 08:16:54 +0200

    change name

--- app/components/Sparcles.tsx
+++ app/components/Sparcles.tsx
@@ -4,9 +4,9 @@ import { SparklesCore } from '../components/ui/sparkles';
 
 export function SparklesPreview() {
 	return (
-		<div className='h-[10rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md'>
+		<div className='h-[12rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md'>
 			<h1 className='md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20'>
-				Aceternity
+				heartless_fpv
 			</h1>
 			<div className='w-[40rem] h-40 relative'>
 				{/* Gradients */}
